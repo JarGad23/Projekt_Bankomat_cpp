74f0b50d97424a133e690b78a2da3ef87f328ff6
JarGad23 <jaroslaw.gad.krypto@gmail.com>
2025-06-09 19:46:52 +0200

    Changes

--- Project_demo/KontoOszczednosciowe.h
+++ Project_demo/KontoOszczednosciowe.h
@@ -5,17 +5,17 @@
 
 class KontoOszczednosciowe : public KontoBankowe {
 private: 
-    int wyp³atyWykoñczone = 0;
-	const int limitWyp³at = 3;
+    int wyplatyWykonczone = 0;
+	const int limitWyplat = 3;
 	const double prowizja = 2.0;
 
 public:
 	bool wyplata(double kwota) override {
-		if (wyp³atyWykoñczone < limitWyp³at) {
+		if (wyplatyWykonczone < limitWyplat) {
 			bool ok = KontoBankowe::wyplata(kwota);
 			if (ok) {
-				wyp³atyWykoñczone++;
-				std::cout << "Wyp³ata wykonana (" << wyp³atyWykoñczone << "/" << limitWyp³at << ").\n";
+				wyplatyWykonczone++;
+				std::cout << "Wyp³ata wykonana (" << wyplatyWykonczone << "/" << limitWyplat << ").\n";
 			}
 			return ok;
 		}
@@ -23,7 +23,7 @@ public:
 			double razem = kwota + prowizja;
 			bool ok = KontoBankowe::wyplata(razem);
 			if (ok) {
-				wyp³atyWykoñczone++;
+				wyplatyWykonczone++;
 				std::cout << "Wyp³ata z prowizj¹ " << prowizja << " PLN (po limicie)\n";
 			}
 			return ok;
